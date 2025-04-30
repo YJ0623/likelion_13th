fc6609f0c4d110e1ecdacb8e4de270e97016e3ab
YJ <hghk0046@gmail.com>
2025-04-30 22:04:13 +0900

    2주차 과제 커밋

--- week2_playlist/eslint.config.js
+++ week2_playlist/eslint.config.js
@@ -9,4 +9,14 @@ export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
   { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"] },
   pluginReact.configs.flat.recommended,
+  {
+    "settings": {
+      "react": {
+        "version": "detect"
+      }
+    },
+    "rules": {
+      "react/react-in-jsx-scope": "off"
+    }
+  }
 ]);
--- week2_playlist/src/pages/Playlist.jsx
+++ week2_playlist/src/pages/Playlist.jsx
@@ -1,25 +1,29 @@
 import albumData from '../datas/song.json';
+import "./Playlist.css";
 
 export const Playlist = () => {
-  const { albumImage, releaseDate, artist, title, lyricist, composer } =
-    albumData;
-
   return (
-    <main className="p-10">
-      <section className="w-100 bg-gray-800 p-6 rounded-lg shadow">
-        <img
-          src={albumImage}
-          alt={`${title} 앨범 커버`}
-          className="w-full rounded mb-4"
-        />
-        <h2 className="text-2xl font-semibold">{title}</h2>
-        <hr className="my-4" />
-        <p className="text-gray-200">🎤 가수: {artist}</p>
-        <p className="text-gray-200">📝 작사가: {lyricist}</p>
-        <p className="text-gray-200">🎼 작곡가: {composer}</p>
-        <p className="text-gray-400 text-sm mt-2">📅 발매일: {releaseDate}</p>
-      </section>
+    <main className="p-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      {albumData.map((item, index) => (
+        <section
+          key={index}
+          className="w-full bg-gray-800 p-6 rounded-lg shadow"
+        >
+          <img
+            src={item.albumImage}
+            alt={`${item.title} 앨범 커버`}
+            className="w-full rounded mb-4"
+          />
+          <h2 className="text-2xl font-semibold">{item.title}</h2>
+          <hr className="my-4" />
+          <p className="text-gray-200">🎤 가수: {item.artist}</p>
+          <p className="text-gray-200">📝 작사가: {item.lyricist}</p>
+          <p className="text-gray-200">🎼 작곡가: {item.composer}</p>
+          <p className="text-gray-400 text-sm mt-2">
+            📅 발매일: {item.releaseDate}
+          </p>
+        </section>
+      ))}
     </main>
   );
 };
-``
